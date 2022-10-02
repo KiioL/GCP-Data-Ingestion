30112fb5ddc521502635d0efdf089c6ff3149db8
unknown <levikitema@gmail.com>
2022-10-03 00:15:43 +0300

    Further edits to executable file

--- interactions20190701-ingestion.sh
+++ interactions20190701-ingestion.sh
@@ -6,12 +6,11 @@ bq mk \
   --description "This is my table" \
   --label organization:development \
   levi_dataset.dmp_data_20190701 \
-  /GCP-Data-Ingestion/myschema.json
 
 bq load \
   --source_format=NEWLINE_DELIMITED_JSON \
   levi_dataset.dmp_data_20190701 \
   "gs://omd-emea-engineer-assessment-dmp/interactions20190701.json" \
-  ./myschema
+  siteNumber:INTEGER,trackId:STRING,timestamp:INTEGER,type:STRING,content:INTEGER,eventId:STRING,variables:JSON,partnerNumber:STRING,partnerCookie:STRING,mergeType:STRING,interactionType:STRING,url:STRING,source:STRING,userAgent:STRING
   "
 
